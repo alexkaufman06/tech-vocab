3812d69b04240b36d9141ccf54077940d996f465
Alex Kaufman and Ben Pritchard <alexkaufman06@gmail.com>
2015-03-10 13:46:45 -0700

    Move difficulty functions to utilities factory

--- controllers/CardCtrl.js
+++ controllers/CardCtrl.js
@@ -6,6 +6,7 @@ techVocab.controller('CardCtrl', [
   'AppFactory',
   function CardCtrl($scope, $state, $stateParams, UtilityFactory, AppFactory){
 
+    $scope.UtilityFactory = UtilityFactory;
     $scope.deck = UtilityFactory.findById(AppFactory.decks, $stateParams.deckId);
     $scope.card = UtilityFactory.findById($scope.deck.cards, $stateParams.cardId);
 
