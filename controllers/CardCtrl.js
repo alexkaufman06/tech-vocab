06b9edb3df0fa5e75fca3da6a1501b2988a0019d
Ben Pritchard and Arsi <ben.s.pritchard@gmail.com>
2015-03-11 12:10:33 -0700

    Add nextCard function

--- controllers/CardCtrl.js
+++ controllers/CardCtrl.js
@@ -10,4 +10,15 @@ techVocab.controller('CardCtrl', [
     $scope.deck = UtilityFactory.findById(AppFactory.decks, $stateParams.deckId);
     $scope.card = UtilityFactory.findById($scope.deck.cards, $stateParams.cardId);
 
+    $scope.nextCard = function() {
+      $state.go('card', { cardId: ($scope.card.id + 1) });
+    }
+
+    // $scope.handleKeyEvent = function(event) {
+    //   console.log('handling it!')
+    //   if (event.which === 78) {
+    //     // var nextCardState = card({ deckId: $scope.deck.id, cardId: ($scope.card.id + 1)})
+    //     $scope.nextCard();
+    //   }
+    // }
 }]);
